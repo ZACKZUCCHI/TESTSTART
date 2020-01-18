5452575093a5ea009a09039de522e1c3e39ce2f6
ZACK LIN <zack.ciaolavoro@gmail.com>
2020-01-18 14:59:53 +0800

    Add Hello Master

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
     public static void main (String[] args) {
 
       System.out.println("HelloWorld! 世界你好!");
+      
+      System.out.println("Hello Master!");
     
     }
 }
